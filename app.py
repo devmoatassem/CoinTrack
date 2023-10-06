8296d0783153a1091fa89113c933762e4770efd8
MOATASSEM BILLAH SANDHU <moatassemsindhu@gmail.com>
2023-10-06 09:11:01 +0500

    about page added

--- app.py
+++ app.py
@@ -363,5 +363,9 @@ def search():
 def contact():
     return render_template("contact.html")
 
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
 if __name__ == "__main__":
     app.run(debug=True)
